7179fb9e1a912aefd3aa5ed924e7194e704c9104
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-09-12 18:00:30 +0530

    Second commit

---
+++ loginScript.js
@@ -0,0 +1,24 @@
+var loginBtn = document.querySelector('.login-btn');
+console.log(loginBtn);
+
+function validation(username, password, callback) {
+    const validUsername = 'admin';
+    const validPassword = '12345';
+
+    username == validUsername && password == validPassword ? callback(true) : callback (false);
+}
+
+loginBtn.addEventListener('click', () => {
+    const username = document.querySelector('#InputEmailLogin').value;
+    const password = document.querySelector('#InputPasswordLogin').value;
+
+    validation(username, password, function(validationValue) {
+        if(validationValue) {
+            window.location.href = "main.html";
+        }
+        else {
+            alert("Invalid username or password!");
+        }
+    });   
+})
+
